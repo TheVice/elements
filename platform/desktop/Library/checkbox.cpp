99cdd0cc422525296468e5f869be9a5875c6a2a8
TheVice <thewinlab@hotmail.com>
2016-09-09 00:01:54 +0300

    [Library] refactored checkbox control.

--- platform/desktop/Library/checkbox.cpp
+++ platform/desktop/Library/checkbox.cpp
@@ -61,254 +61,127 @@ void checkbox::draw()
 {
 	const auto pos = global_position();
 	const auto step = eps::math::vec2(get_size().x / DEFAULT_WIDTH, get_size().y / DEFAULT_WIDTH);
-	EPS_STATE_BLEND(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	EPS_STATE_PROGRAM(program_face_.get_product());
-	program_face_.uniform_value(eps::utils::to_int(program_enum::u_transform), get_system()->transform());
-	program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[0]);
-	auto minPos = eps::math::vec2(pos.x, pos.y);
-	auto maxPos = eps::math::vec2(pos.x + get_size().x, pos.y + step.y);
+	//
+	std::vector<std::pair<eps::math::vec4, eps::math::vec4>> pos_and_colors =
 	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	minPos = eps::math::vec2(pos.x + get_size().x - step.x, pos.y + step.y);
-	maxPos = eps::math::vec2(pos.x + get_size().x, pos.y + get_size().y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[1]);
-	minPos = eps::math::vec2(pos.x, pos.y + get_size().y - step.y);
-	maxPos = eps::math::vec2(pos.x + get_size().x - step.x, pos.y + get_size().y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	minPos = eps::math::vec2(pos.x, pos.y + step.y);
-	maxPos = eps::math::vec2(pos.x + step.x, pos.y + get_size().y - step.y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[2]);
-	minPos = eps::math::vec2(pos.x + step.x, pos.y + step.y);
-	maxPos = eps::math::vec2(pos.x + get_size().x - step.x, pos.y + 2 * step.y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	minPos = eps::math::vec2(pos.x + get_size().x - 2 * step.x, pos.y + 2 * step.y);
-	maxPos = eps::math::vec2(pos.x + get_size().x - step.x, pos.y + get_size().y - step.y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[3]);
-	minPos = eps::math::vec2(pos.x + step.x, pos.y + get_size().y - 2 * step.y);
-	maxPos = eps::math::vec2(pos.x + get_size().x - 2 * step.x, pos.y + get_size().y - step.y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
-	minPos = eps::math::vec2(pos.x + step.x, pos.y + 2 * step.y);
-	maxPos = eps::math::vec2(pos.x + 2 * step.x, pos.y + get_size().y - 2 * step.y);
-	{
-		GLfloat vertices[] =
-		{
-			(minPos.x), (minPos.y), 0.0f, 1.0f,
-			(maxPos.x), (minPos.y), 1.0f, 1.0f,
-			(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-			(minPos.x), (maxPos.y), 0.0f, 0.0f
-		};
-		square_.construct(vertices);
-	}
-	square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-				   eps::utils::to_int(program_enum::a_vertex_uv));
+		std::make_pair(
+			eps::math::vec4(pos.x, pos.y,
+		pos.x + get_size().x, pos.y + step.y),
+			colors_[0]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x + get_size().x - step.x, pos.y + step.y,
+		pos.x + get_size().x, pos.y + get_size().y),
+			colors_[0]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x, pos.y + get_size().y - step.y,
+		pos.x + get_size().x - step.x, pos.y + get_size().y),
+			colors_[1]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x, pos.y + step.y,
+		pos.x + step.x, pos.y + get_size().y - step.y),
+			colors_[1]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x + step.x, pos.y + step.y,
+		pos.x + get_size().x - step.x, pos.y + 2 * step.y),
+			colors_[2]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x + get_size().x - 2 * step.x, pos.y + 2 * step.y,
+		pos.x + get_size().x - step.x, pos.y + get_size().y - step.y),
+			colors_[2]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x + step.x, pos.y + get_size().y - 2 * step.y,
+		pos.x + get_size().x - 2 * step.x, pos.y + get_size().y - step.y),
+			colors_[3]),
+		//
+		std::make_pair(
+			eps::math::vec4(pos.x + step.x, pos.y + 2 * step.y,
+		pos.x + 2 * step.x, pos.y + get_size().y - 2 * step.y),
+			colors_[3])
+	};
 
 	switch (state_)
 	{
 		case state::CHECKED:
 		{
-			program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[4]);
-			minPos = eps::math::vec2(pos.x + 3 * step.x, pos.y + 5 * step.y);
-			maxPos = eps::math::vec2(pos.x + 4 * step.x, pos.y + 8 * step.y);
+			std::vector<std::pair<eps::math::vec4, eps::math::vec4>> checked_pos_and_colors =
 			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 4 * step.x, pos.y + 4 * step.y);
-			maxPos = eps::math::vec2(pos.x + 5 * step.x, pos.y + 7 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 5 * step.x, pos.y + 3 * step.y);
-			maxPos = eps::math::vec2(pos.x + 6 * step.x, pos.y + 6 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 6 * step.x, pos.y + 4 * step.y);
-			maxPos = eps::math::vec2(pos.x + 7 * step.x, pos.y + 7 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 7 * step.x, pos.y + 5 * step.y);
-			maxPos = eps::math::vec2(pos.x + 8 * step.x, pos.y + 8 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 8 * step.x, pos.y + 6 * step.y);
-			maxPos = eps::math::vec2(pos.x + 9 * step.x, pos.y + 9 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
-			minPos = eps::math::vec2(pos.x + 9 * step.x, pos.y + 7 * step.y);
-			maxPos = eps::math::vec2(pos.x + 10 * step.x, pos.y + 10 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
+				std::make_pair(
+					eps::math::vec4(pos.x + 3 * step.x, pos.y + 5 * step.y,
+				pos.x + 4 * step.x, pos.y + 8 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 4 * step.x, pos.y + 4 * step.y,
+				pos.x + 5 * step.x, pos.y + 7 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 5 * step.x, pos.y + 3 * step.y,
+				pos.x + 6 * step.x, pos.y + 6 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 6 * step.x, pos.y + 4 * step.y,
+				pos.x + 7 * step.x, pos.y + 7 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 7 * step.x, pos.y + 5 * step.y,
+				pos.x + 8 * step.x, pos.y + 8 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 8 * step.x, pos.y + 6 * step.y,
+				pos.x + 9 * step.x, pos.y + 9 * step.y),
+					colors_[4]),
+				//
+				std::make_pair(
+					eps::math::vec4(pos.x + 9 * step.x, pos.y + 7 * step.y,
+				pos.x + 10 * step.x, pos.y + 10 * step.y),
+					colors_[4])
+			};
+			pos_and_colors.insert(pos_and_colors.end(), checked_pos_and_colors.begin(), checked_pos_and_colors.end());
 		}
 		break;
 
 		case state::INDETERMINATE:
 		{
-			program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), colors_[4]);
-			minPos = eps::math::vec2(pos.x + 3 * step.x, pos.y + 3 * step.y);
-			maxPos = eps::math::vec2(pos.x + 10 * step.x, pos.y + 10 * step.y);
-			{
-				GLfloat vertices[] =
-				{
-					(minPos.x), (minPos.y), 0.0f, 1.0f,
-					(maxPos.x), (minPos.y), 1.0f, 1.0f,
-					(maxPos.x), (maxPos.y), 1.0f, 0.0f,
-					(minPos.x), (maxPos.y), 0.0f, 0.0f
-				};
-				square_.construct(vertices);
-			}
-			square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
-						   eps::utils::to_int(program_enum::a_vertex_uv));
+			pos_and_colors.push_back(std::make_pair(
+										 eps::math::vec4(pos.x + 3 * step.x, pos.y + 3 * step.y,
+												 pos.x + 10 * step.x, pos.y + 10 * step.y),
+										 colors_[4]));
 		}
 		break;
 
 		default:
 			break;
+	}
+
+	EPS_STATE_BLEND(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	EPS_STATE_PROGRAM(program_face_.get_product());
+	program_face_.uniform_value(eps::utils::to_int(program_enum::u_transform), get_system()->transform());
+
+	for (const auto& color_position : pos_and_colors)
+	{
+		program_face_.uniform_value(eps::utils::to_int(program_enum::u_color), std::get<1>(color_position));
+		//
+		GLfloat vertices[] =
+		{
+			std::get<0>(color_position).x, std::get<0>(color_position).y, 0.0f, 1.0f,
+			std::get<0>(color_position).z, std::get<0>(color_position).y, 1.0f, 1.0f,
+			std::get<0>(color_position).z, std::get<0>(color_position).w, 1.0f, 0.0f,
+			std::get<0>(color_position).x, std::get<0>(color_position).w, 0.0f, 0.0f
+		};
+		//
+		square_.construct(vertices);
+		square_.render(program_face_, eps::utils::to_int(program_enum::a_vertex_xy),
+					   eps::utils::to_int(program_enum::a_vertex_uv));
 	}
 
 	control::draw();
